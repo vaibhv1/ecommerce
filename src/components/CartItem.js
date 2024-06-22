666952a18be095352a385ed500665407be7bb5d0
vaibhav04861 <vaibhavbv1@gmail.com>
2024-06-22 23:02:40 +0530

    cart changes

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,17 +1,30 @@
 import React from 'react';
 
-const CartItem = ({ item, removeFromCart, updateQuantity }) => (
-  <div className="cart-item">
-    <img src={item.image} alt={item.title} />
-    <h3>{item.title}</h3>
-    <p>${item.price}</p>
-    <input
-      type="number"
-      value={item.quantity}
-      onChange={(e) => updateQuantity(item.id, parseInt(e.target.value))}
-    />
-    <button onClick={() => removeFromCart(item.id)}>Remove</button>
-  </div>
-);
+const CartItem = ({ item, removeFromCart, updateQuantity }) => {
+  
+  const handleQuantityChange = (e) => {
+    const newQuantity = parseInt(e.target.value);
+    updateQuantity(item.id, newQuantity);
+    
+    // Remove the item if quantity is set to zero
+    if (newQuantity === 0) {
+      removeFromCart(item.id);
+    }
+  };
+
+  return (
+    <div className="cart-item">
+      <img src={item.image} alt={item.title} />
+      <h3>{item.title}</h3>
+      <p>${item.price}</p>
+      <input
+        type="number"
+        value={item.quantity}
+        onChange={handleQuantityChange}
+      />
+      <button onClick={() => removeFromCart(item.id)}>Remove</button>
+    </div>
+  );
+};
 
 export default CartItem;
